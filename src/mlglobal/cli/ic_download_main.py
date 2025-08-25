df283a0a9c455afb15095f86f1e0f180b0fc4c00
Rahul Mahajan <aerorahul@users.noreply.github.com>
2025-08-25 17:21:23 -0400

    wip on the downloader, seems close

--- src/mlglobal/cli/ic_download_main.py
+++ src/mlglobal/cli/ic_download_main.py
@@ -1,26 +1,58 @@
+import os
 import argparse
+
 from datetime import datetime
+from pathlib import Path
+
 from mlglobal.ic_downloader import ICDownloader
 
+# Default bucket and root directory for each mode  # TODO: store this in a yaml or some config file
+DEFAULTS = {
+    "gfs": {
+        "bucket_name": "noaa-gfs-bdp-pds",
+        "root_directory": "gdas",
+    },
+    "gefs": {
+        "bucket_name": "noaa-ncepdev-none-ca-ufs-cpldcld",
+        "root_directory": "gefs",
+    }
+}
+
 def main():
-    parser = argparse.ArgumentParser(description="Download IC data (GEFS/GFS)")
-    parser.add_argument('--mode', choices=['gefs', 'gfs'], required=True)
-    parser.add_argument('--start', required=True, help='Start datetime (YYYYMMDDHH)')
-    parser.add_argument('--end', required=True, help='End datetime (YYYYMMDDHH)')
-    parser.add_argument('--member', help='Ensemble member (for GEFS)')
-    parser.add_argument('--download-source', choices=['s3', 'nomads'], default='s3')
-    parser.add_argument('--download-directory', default=None)
+    parser = argparse.ArgumentParser(description="Download IC data for GFS or GEFS")
+
+    subparsers = parser.add_subparsers(dest='mode', help="System to download IC data for", type=str, choices=["gfs", "gefs"], required=True)
+
+    def _common_args(inparser, dict_in):
+        inparser.add_argument("--start_date", help="Start datetime", type=str, required=True)
+        inparser.add_argument("--end_date", help="End datetime", type=str, required=True)
+        inparser.add_argument("--levels", help="number of pressure levels", type=int, choices=[13, 37], default=13, required=False)
+        inparser.add_argument("--source", help="Data source", type=str, choices=["s3", "nomads", "local"], default="s3", required=False)
+        inparser.add_argument("--target", help="Target directory to store raw data into", type=str, default=os.getcwd(), required=False)
+        inparser.add_argument("--bucket-name", help="S3 bucket name", type=str, default=dict_in["bucket_name"], required=False)
+        inparser.add_argument("--root-directory", help="Root directory", type=str, default=dict_in["root_directory"], required=False)
+        return inparser
+
+    # GFS subparser
+    gfs_parser = subparsers.add_parser("gfs", help="Download GFS ensemble data")
+    gfs_parser = _common_args(gfs_parser, DEFAULTS["gfs"])
+
+    # GEFS subparser
+    gefs_parser = subparsers.add_parser("gefs", help="Download GEFS ensemble data")
+    gefs_parser = _common_args(gefs_parser, DEFAULTS["gefs"])
+    gefs_parser.add_argument("--member", help="Ensemble member", type=int, choices=list(range(0, 31)), default=0)
+
     args = parser.parse_args()
 
-    start_dt = datetime.strptime(args.start, '%Y%m%d%H')
-    end_dt = datetime.strptime(args.end, '%Y%m%d%H')
     downloader = ICDownloader(
         mode=args.mode,
-        start_datetime=start_dt,
-        end_datetime=end_dt,
-        member=args.member,
-        download_source=args.download_source,
-        download_directory=args.download_directory
+        start_datetime=datetime.fromisoformat(args.start_date),
+        end_datetime=datetime.fromisoformat(args.end_date),
+        member=None if args.mode == "gfs" else args.member,
+        download_source=args.source,
+        download_directory=args.target,
+        bucket_name=args.bucket_name,
+        root_directory=args.root_directory
     )
     downloader.download()
 
--- src/mlglobal/ic_downloader.py
+++ src/mlglobal/ic_downloader.py
@@ -1,99 +1,234 @@
 import os
-import requests
-import boto3
-from datetime import datetime
+import glob
+from datetime import datetime, timedelta
+
+class FileFormats:
+    def __init__(self, mode, num_levels=13):
+
+        FILE_FORMATS = {
+            "gfs": self.gfs_file_formats,
+            "gefs": self.gefs_file_formats
+        }
+        self.num_levels = num_levels
+        self.file_formats = FILE_FORMATS[mode]()
+
+    def gfs_file_formats(self):
+         # List of file formats to download
+        if self.num_levels == 13:
+           file_formats = ['pgrb2.0p25.f000', 'pgrb2.0p25.f006'] # , '0p25.f001'
+        else:
+            file_formats = ['pgrb2.0p25.f000', 'pgrb2b.0p25.f000', 'pgrb2.0p25.f006'] # , '0p25.f001'
+
+        return file_formats
+
+    def gefs_file_formats(self):
+
+        # List of file formats to download
+        if self.num_levels == 13:
+            file_formats = ['pgrb2.0p25.f000', 'pgrb2s.0p25.f000'] # , '0p25.f001'
+        else:
+            file_formats = ['pgrb2.0p25.f000', 'pgrb2b.0p25.f000', 'pgrb2.0p25.f006'] # , '0p25.f001'
+
+        return file_formats
+
 
 class ICDownloader:
-    MODES = {
-        'gefs': {
-            'bucket_name': 'noaa-ncepdev-none-ca-ufs-cpldcld',
-            'root_directory': 'gefs',
-        },
-        'gfs': {
-            'bucket_name': 'noaa-gfs-bdp-pds',
-            'root_directory': 'gdas',
-        }
-    }
 
-    def __init__(self, mode, start_datetime, end_datetime, member=None, download_source='s3', download_directory=None):
+    def __init__(
+        self,
+        mode,
+        start_datetime,
+        end_datetime,
+        member=None,
+        num_pressure_levels=13,
+        download_source="s3",
+        download_directory=None,
+        bucket_name=None,
+        root_directory=None
+    ):
         self.mode = mode
         self.start_datetime = start_datetime
         self.end_datetime = end_datetime
         self.member = member
+        self.num_pressure_levels = num_pressure_levels
         self.download_source = download_source
-        self.bucket_name = self.MODES[mode]['bucket_name']
-        self.root_directory = self.MODES[mode]['root_directory']
-        self.download_directory = download_directory or os.getcwd()
+        self.download_directory = download_directory
+        self.bucket_name = bucket_name
+        self.root_directory = root_directory
 
-    def s3bucket(self, date_str, time_str, local_directory):
-        if self.mode == 'gefs':
-            member_str = f"ge{self.member}"
-        else:
-            member_str = "gdas"
-        s3_prefix = f"Linlin.Cui/gefs_wcoss2/{self.root_directory}.{date_str}/{time_str}/atmos/"
-        def get_data(s3_prefix, file_format, local_directory):
-            s3_objects = self.s3.list_objects_v2(Bucket=self.bucket_name, Prefix=s3_prefix)
-            for obj in s3_objects.get('Contents', []):
-                obj_key = obj['Key']
-                if obj_key.endswith(f'{file_format}'):
-                    local_file_path = os.path.join(local_directory, os.path.basename(obj_key))
-                    self.s3.download_file(self.bucket_name, obj_key, local_file_path)
-                    print(f"Downloaded {obj_key} to {local_file_path}")
-        for file_format in self.file_formats:
-            if self.mode == 'gefs':
-                curr_file = f"ge{self.member}.t{time_str}z.{file_format}"
+        ff = FileFormats(mode, num_levels=self.num_pressure_levels)
+        self.file_formats = ff.file_formats
+
+        self.s3 = self.init_s3_client() if self.download_source == "s3" else None
+
+        self.DOWNLOAD_METHODS = {
+            "s3": self.s3bucket,
+            "local": self.localarchive
+        }
+
+    def get_s3_specs(self, ymd, hh, file_format):
+
+        if self.mode == "gefs":
+
+            s3_prefix = f"Linlin.Cui/gefs_wcoss2/{self.root_directory}.{ymd}/{hh}/atmos/"
+            s3_file_format = f"{self.member}.t{hh}z.{file_format}"
+
+        elif self.mode == "gfs":
+
+            if file_format == "pgrb2.0p25.f006":
+                # get prefix for precip from the previous cycle
+                # Convert ymd and hh to datetime object
+                datetime_obj = datetime.strptime(ymd + hh, "%Y%m%d%H")
+
+                # Get the datetime 6 hours before
+                datetime_before = datetime_obj - timedelta(hours=6)
+
+                # Get the date string and time string from datetime objects
+                ymd_precip = datetime_before.strftime("%Y%m%d")
+                hh_precip = datetime_before.strftime("%H")
+
+                # Construct the S3 prefix for the directory
+                s3_prefix = f"{self.root_directory}.{ymd_precip}/{hh_precip}/"
+
             else:
-                curr_file = f"gdas.t{time_str}z.{file_format}"
-            get_data(s3_prefix, curr_file, local_directory)
 
-    def archive(self, date_str, time_str, local_directory):
-        data_path = "/lfs/h2/emc/ptmp/jun.wang"
-        def get_data(data_path, file_format, local_directory):
-            file_objects = glob.glob(f"{data_path}/{self.root_directory}.{date_str}/{time_str}/*/*/*")
-            for obj_key in file_objects:
-                if obj_key.endswith(f'{file_format}'):
-                    local_file_path = os.path.join(local_directory, os.path.basename(obj_key))
-                    try:
-                        os.symlink(obj_key, local_file_path)
-                        print(f"Symbolic link created: {obj_key} -> {local_directory}")
-                    except OSError as e:
-                        print(f"Error creating symbolic link: {e}")
+                s3_prefix = f"{self.root_directory}.{ymd}/{hh}/"
+
+            s3_file_format = file_format
+
+        return s3_prefix, s3_file_format
+
+    def get_data_from_s3(self, path_prefix: str,
+                        file_format: str, local_directory: str) -> None:
+        """
+        Downloads files with a specific format from an S3 bucket to a local directory.
+
+        Parameters
+        ----------
+        prefix : str
+            The prefix (folder path) in the S3 bucket to filter objects.
+        file_format : str
+            The file extension or format to filter files (e.g., '.csv', '.json').
+        local_directory : str
+            The local directory path where the downloaded files will be saved.
+
+        Returns
+        -------
+        None
+            This function does not return anything. Files are downloaded as a side effect.
+
+        Notes
+        -----
+        Only files ending with the specified `file_format` will be downloaded.
+        """
+
+        objects = self.s3.list_objects_v2(Bucket=self.bucket_name, Prefix=path_prefix)
+        for obj in objects.get('Contents', []):
+            obj_key = obj['Key']
+            if obj_key.endswith(f'{file_format}'):
+                local_file_path = os.path.join(local_directory, os.path.basename(obj_key))
+                self.s3.download_file(self.bucket_name, obj_key, local_file_path)
+                print(f"Downloaded {obj_key} to {local_file_path}")
+
+    def get_local_specs(self, ymd: str, hh: str, file_format: str) -> tuple[str, str]:
+        """
+        Get the local specifications for a given date, time, and file format.
+
+        Parameters
+        ----------
+        ymd : str
+            The date string in the format 'YYYYMMDD'.
+        hh : str
+            The time string in the format 'HH'.
+        file_format : str
+            The file format string (e.g., 'pgrb2.0p25.f006').
+
+        Returns
+        -------
+        tuple[str, str]
+            A tuple containing the local path prefix and the local file format.
+        """
+        if self.mode == "gefs":
+            local_prefix = f"/lfs/h2/emc/ptmp/jun.wang/gefs.{ymd}/{hh}"
+            local_file_format = f"{self.member}.t{hh}z.{file_format}"
+        elif self.mode == "gfs":
+            local_prefix = f"/lfs/h1/ops/prod/com/gfs/v16.3/gfs.{ymd}/{hh}/00/atmos"
+            local_file_format = f"gfs.t{hh}z.{file_format}"
+
+        return local_prefix, local_file_format
+
+    def get_data_from_local(self, path_prefix: str,
+                        file_format: str, local_directory: str) -> None:
+
+        file_objects = glob.glob(f"{path_prefix}/*/*/*")
+        for obj_key in file_objects:
+            if obj_key.endswith(f'{file_format}'):
+
+                # Define the local file path
+                local_file_path = os.path.join(local_directory, os.path.basename(obj_key))
+
+                # Move data to the local path
+                try:
+                    os.symlink(obj_key, local_file_path)
+                    print(f"Symbolic link created: {obj_key} -> {local_directory}")
+                except OSError as e:
+                    raise OSError(f"Error creating symbolic link: {e}")
+
+        return
+
+    @staticmethod
+    def init_s3_client():
+
+        try:
+            import boto3
+            from botocore.config import Config
+            from botocore import UNSIGNED
+        except ImportError as ee:
+            raise ImportError("boto3 and botocore are required for S3 operations.") from ee
+
+        try:
+            s3 = boto3.client("s3", config=Config(signature_version=UNSIGNED))
+        except Exception as e1:
+            try:
+                profile_name = os.environ.get('AWS_PROFILE', 'default')
+                session = boto3.Session(profile_name=profile_name)
+                current_credentials = session.get_credentials().get_frozen_credentials()
+                s3 = session.client("s3", aws_access_key_id=current_credentials.access_key,
+                                    aws_secret_access_key=current_credentials.secret_key)
+            except Exception as e2:
+                raise RuntimeError("Failed to create S3 client with both unsigned and profile methods.") from e2
+
+            return s3
+
+    def s3bucket(self, ymd, hh, local_directory):
+
         for file_format in self.file_formats:
-            if self.mode == 'gefs':
-                curr_file = f"ge{self.member}.t{time_str}z.{file_format}"
-            else:
-                curr_file = f"gdas.t{time_str}z.{file_format}"
-            get_data(data_path, curr_file, local_directory)
+            s3_prefix, s3_file_format = self.get_s3_specs(ymd, hh, file_format)
+            self.get_data_from_s3(s3_prefix, s3_file_format, local_directory)
 
-    def download(self):
-        import boto3
-        from datetime import timedelta
-        profile_name = os.environ.get('AWS_PROFILE', 'default')
-        session = boto3.Session(profile_name=profile_name)
-        current_credentials = session.get_credentials().get_frozen_credentials()
-        self.s3 = session.client(
-            's3',
-            aws_access_key_id=current_credentials.access_key,
-            aws_secret_access_key=current_credentials.secret_key,
-        )
-        if self.mode == 'gefs':
-            self.file_formats = ['pgrb2.0p25.f000', 'pgrb2s.0p25.f000'] if self.member and self.member.isdigit() and int(self.member) <= 30 else ['pgrb2.0p25.f000', 'pgrb2b.0p25.f000', 'pgrb2.0p25.f006']
-        else:
-            self.file_formats = ['pgrb2.0p25.f000', 'pgrb2.0p25.f006']
+    def localarchive(self, ymd, hh, local_directory):
+
+        for file_format in self.file_formats:
+            local_prefix, local_file_format = self.get_local_specs(ymd, hh, file_format)
+            self.get_data_from_local(local_prefix, local_file_format, local_directory)
+
+    def download(self, loop_interval=6):
+
+        # Loop through the intervals
         current_datetime = self.start_datetime
         while current_datetime <= self.end_datetime:
-            date_str = current_datetime.strftime("%Y%m%d")
-            time_str = current_datetime.strftime("%H")
-            if self.mode == 'gefs':
-                local_directory = os.path.join(self.download_directory or os.getcwd(), self.bucket_name+f'_{len(self.file_formats)}_{self.member}', date_str, time_str)
-            else:
-                local_directory = os.path.join(self.download_directory or os.getcwd(), self.bucket_name+f'_{len(self.file_formats)}', date_str, time_str)
+            ymd = current_datetime.strftime("%Y%m%d")
+            hh = current_datetime.strftime("%H")
+
+            # Define the local directory path where the file will be saved
+            local_directory = os.path.join(self.local_base_directory, ymd, hh)
+
+            # Create the local directory if it doesn't exist
             os.makedirs(local_directory, exist_ok=True)
-            if self.download_source == 's3':
-                self.s3bucket(date_str, time_str, local_directory)
-            elif self.download_source == 'wcoss2':
-                self.archive(date_str, time_str, local_directory)
-            else:
-                raise ValueError(f'data source {self.download_source} is not supported, choose either s3 or wcoss2!')
-            current_datetime += timedelta(hours=6)
+
+            self.DOWNLOAD_METHODS[self.download_source](ymd, hh, local_directory)
+
+            # Move to the next interval
+            current_datetime += timedelta(hours=loop_interval)
+
         print("Download completed.")
